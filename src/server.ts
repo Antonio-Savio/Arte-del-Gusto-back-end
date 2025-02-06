64e84930fdd200312f5bedf066055f35a5e60bc5
Antonio-Savio <antonio.savio28@gmail.com>
2025-02-06 05:12:02 -0300

    chore: setted an environment variable for the port

--- src/server.ts
+++ src/server.ts
@@ -35,4 +35,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     return;
 })
 
-app.listen(3333, () => console.log("Servidor online ns porta 3333"))
+app.listen(process.env.PORT, () => console.log("Servidor online ns porta 3333"))
